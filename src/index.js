ce18194631c68a113824f3679fab10c283e58be1
rose_noire <pp75vannier@gmail.com>
2026-08-16 18:56:33 +0200

    Add Render web server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 require("dotenv").config();
+const http = require("http");
 
+const PORT = process.env.PORT || 10000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+
+  res.end("BADG3D Custom Bot is online!");
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`🌐 Serveur HTTP lancé sur le port ${PORT}`);
+});
 const {
   Client,
   GatewayIntentBits,
